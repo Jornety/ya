b92d68a50863cd73815cb35f23192e7ef4f151ff
Jornety <armajabayu@gmail.com>
2024-10-14 10:52:07 +0800

    Add files via upload

---
+++ companyprofile/about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perusahaan A1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="wrapper">
+            <!---taskbar-->
+            <div class="circle"></div>
+            <div class="logo"><a href="#">Perusahaan web</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="index.html#home">Home</a></li>
+                    <li><a href="index.html#services">Services</a></li>
+                    <li><a href="index.html#joinus">Join Us</a></li>
+                    <li><a href="index.html#contact">Contact</a></li>
+                    <li><a href="about.html#about">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <section id="about">
+        <div class="kolom">
+            <p class="deskripsi">About</p>
+            <h2>All About Us!</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatem laborum nihil ab libero,
+                rerum omnis, hic <br> alias cupiditate
+                magni velit quia amet earum possimus temporibus inventore sed <br> culpa eligendi reprehenderit veniam
+                consequatur 
+                cum fugiat eum ipsa. <br>Nisi omnis, consectetur, saepe nulla nobis corporis neque libero, cupiditate 
+                recusandae soluta laboriosam dolore. <br> Soluta
+                ut culpa vel illum aspernatur? Rerum dolores quibusdam, beatae sunt, <br>dignissimos tempora suscipit
+                molestias modi minus, quo labore?</p>
+        </div>
+
+        <img src="tentang.jpg" alt="" srcset="" width="700px">
+    </section>
+
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2024. <b>Perusahaan Web.</b> All Rights Reserved.
+        </div>
+    </div>
+
+</body>
+
+</html>
